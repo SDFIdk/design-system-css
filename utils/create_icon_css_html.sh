1e0f6f5f517f992da885fbe6253bf028918b732e
Frank R Thomsen <frank.thomsen@gmail.com>
2022-07-15 13:15:35 +0200

    change icon color architecture

---
+++ utils/create_icon_css_html.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Generates a piece of CSS code that includes all the available SVG icons
+
+echo '/* Icons */' > icons.css
+echo ':root {' >> icons.css
+
+for i in ../node_modules/design-system-icons/icons/*.svg; do
+  svgstr1=$(cat ${i})
+  svgstr2=${svgstr1//black/#3EDDC6}
+  svgstr3=${svgstr2//#000/#3EDDC6}
+  svgstr4=$(node convert_svg.js "$svgstr3")
+  echo "--${i:42:(-4)}: url('data:image/svg+xml;utf8,${svgstr4}');" >> icons.css
+done
+
+echo '}' >> icons.css
+
+for i in ../node_modules/design-system-icons/icons/*.svg; do
+  svgstr1=$(cat ${i})
+  svgstr2=$(node convert_svg.js "$svgstr1")
+  echo ".ds-icon-${i:42:(-4)}::before { background-image: var(--${i:42:(-4)});}" >> icons.css
+done
+
+echo "CSS snippet generated in icons.css"
+
+
+# Generates a piece of HTML code that lists all the available icons
+
+echo '<ul class="ds-grid icon-list">' > icons.html
+
+for i in ../node_modules/design-system-icons/icons/*.svg; do
+  echo "<li class=\"icon\"><span class=\"ds-icon-${i:42:(-4)}\"></span><code>&lt;span class=\"ds-icon-${i:42:(-4)}\">&lt;/span></code></li>" >> icons.html
+done
+
+echo "</ul>" >> icons.html
+echo "HTML snippet generated in icons.html"
